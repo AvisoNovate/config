b4ea241d85ad0f449d1737a5a266138dc99f8d3e
Howard M. Lewis Ship <hlship@walmartlabs.com>
2016-07-15 08:41:08 -0700

    A little OCD renaming/reordering of the test specs

--- spec/config_specs.clj
+++ spec/config_specs.clj
@@ -52,7 +52,7 @@
            (should= {:web-server {:port      9090
                                   :pool-size 40}})))
 
-  (it "mixes together multiple profiles and schemas"
+  (it "mixes together multiple profiles and overrides"
       (->> (assemble-configuration {:profiles         [:mix]
                                     :additional-files ["dev-resources/mix-production-overrides.edn"]
                                     :overrides        {:web-server {:port 9999}}})
@@ -62,36 +62,26 @@
                                   :user     "prod"
                                   :password "secret"}})))
 
-  (it "processes arguments"
-      (->> (assemble-configuration {:profiles [:mix]
-                                    :args     ["--load" "dev-resources/mix-production-overrides.edn"]})
-           (should= {:web-server {:port      8000
-                                  :pool-size 100}
-                     :database   {:hostname "prod-db"
-                                  :user     "prod"
-                                  :password "secret"}})))
+  (context ":args options"
+    (it "processes arguments"
+        (->> (assemble-configuration {:profiles [:mix]
+                                      :args     ["--load" "dev-resources/mix-production-overrides.edn"]})
+             (should= {:web-server {:port      8000
+                                    :pool-size 100}
+                       :database   {:hostname "prod-db"
+                                    :user     "prod"
+                                    :password "secret"}})))
 
-  (it "reports unexpected arguments"
-      (let [e (should-throw ExceptionInfo
-                            (assemble-configuration {:args ["--load" "override.edn" "xxx"]}))]
-        (should= "Unexpected command line argument." (.getMessage e))
-        (should= {:reason    :io.aviso.config/command-line-parse-error
-                  :argument  "xxx"
-                  :arguments ["--load" "override.edn" "xxx"]}
-                 (ex-data e))))
+    (it "reports unexpected arguments"
+        (let [e (should-throw ExceptionInfo
+                              (assemble-configuration {:args ["--load" "override.edn" "xxx"]}))]
+          (should= "Unexpected command line argument." (.getMessage e))
+          (should= {:reason    :io.aviso.config/command-line-parse-error
+                    :argument  "xxx"
+                    :arguments ["--load" "override.edn" "xxx"]}
+                   (ex-data e)))))
 
-  (it "can build a system"
-      (should= 9999
-               (let [system (component/system-map :web-server (new-web-server))
-                     configuration (assemble-configuration {:variants [:system :local]
-                                                            :profiles [:web-server]})]
-                 (-> system
-                     (configure-components configuration)
-                     component/start-system
-                     :web-server
-                     :port))))
-
-  (context "EDN parsing"
+  (context "EDN reader macros"
 
     (context "#config/prop"
 
@@ -133,6 +123,18 @@
                          :prop-value   "<kazzam>"})))))
 
   (context "configure-components"
+
+    (it "can build a system"
+        (should= 9999
+                 (let [system        (component/system-map :web-server (new-web-server))
+                       configuration (assemble-configuration {:variants [:system :local]
+                                                              :profiles [:web-server]})]
+                   (-> system
+                       (configure-components configuration)
+                       component/start-system
+                       :web-server
+                       :port))))
+
     (it "can associate configuration for a plain map component"
         (let [component-configuration {:foo :bar}
               component               (with-config-spec {:name :this-component} :this-component some?)
@@ -140,7 +142,7 @@
               configured-system       (configure-components system-map {:this-component component-configuration})]
           (should-be-same component-configuration (-> configured-system :this-component :configuration))))
 
-    (it "can invoke Configurable/configure if implemented"
+    (it "will invoke Configurable/configure if implemented"
         (let [component-configuration {:foo :bar}
               component               (with-config-spec (map->Capturing {})
                                                         :that-component some?)
