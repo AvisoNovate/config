206d30c4f55a4dd936fd9d4ae926defb1794ef24
Howard M. Lewis Ship <hlship@walmartlabs.com>
2016-07-15 10:02:35 -0700

    Document more of io.aviso.config.spec, make dictionary a macro

--- src/io/aviso/config/spec.clj
+++ src/io/aviso/config/spec.clj
@@ -1,5 +1,13 @@
 (ns io.aviso.config.spec
-  "Utilities related to clojure.spec."
+  "Utilities related to clojure.spec.
+
+  This namespace defines two resuable specs:
+
+  ::unqualified-keyword
+  : A keyword that has no namespace.
+
+  ::qualified-keyword
+  : A keyword that must have a namespace."
   {:added "0.2.0"}
   (:require [clojure.spec :as s]
             [clojure.spec.gen :as gen]))
@@ -16,9 +24,11 @@
            #(-> % namespace some?))
     gen/keyword-ns))
 
-(defn dictionary
-  "A dictionary is a spec that maps arbitrary keywords to a particular spec."
+(defmacro dictionary
+  "A dictionary is a spec that maps arbitrary keywords to a particular spec.
+
+  By default, keys in the dictionary map conform to ::unqualified-keyword."
   ([value-spec]
-   (dictionary ::unqualified-keyword value-spec))
+   `(dictionary ::unqualified-keyword ~value-spec))
   ([key-spec value-spec]
-   (s/every-kv key-spec value-spec)))
+   `(s/every-kv ~key-spec ~value-spec)))
